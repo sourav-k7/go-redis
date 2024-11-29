332cef31708fd1e1da4605b05182d6476d37971a
sourav kumar <70101213+sourav-k7@users.noreply.github.com>
2024-11-29 23:39:01 +0530

    go program to store key value in cache

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sourav-k7/go-redis
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Cache struct {
+	cache map[string]string
+}
+
+func (store Cache) SetCache(key string, value string) (bool, error) {
+	store.cache[key] = value
+	return true, nil
+}
+
+func (store Cache) GetCache(key string) (string, bool) {
+	value, exist := store.cache[key]
+	if !exist {
+		return "", exist
+	}
+	return value, true
+}
+
+func (store Cache) DeleteCache(key string){
+	delete(store.cache,key);
+}
+
+func main() {
+	cache := Cache{
+		cache: make(map[string]string),
+	}
+	cache.SetCache("test", "val")
+	value, exist := cache.GetCache("test")
+	if exist {
+		fmt.Println(value)
+	}
+	cache.DeleteCache("test")
+}
